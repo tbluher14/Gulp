0a967ae1dcaf55a88f2dba6f43639dee44a3ad77
Willard Marett <will.j.marett@gmail.com>
2022-10-14 17:34:40 -0500

    Began forms

--- app/api/business_routes.py
+++ app/api/business_routes.py
@@ -10,3 +10,7 @@ def get_all_businesses():
   businesses = Business.query.all()
   print('THIS IS BUSINESS', businesses)
   return {'businesses': [business.to_dict() for business in businesses]}
+
+@business_routes.route('/create_business', methods=['POST'])
+def create_business():
+  pass
