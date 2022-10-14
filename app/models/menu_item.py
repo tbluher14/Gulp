d47a91521eff50a611aaa395751640b52c943763
Willard Marett <will.j.marett@gmail.com>
2022-10-14 12:53:10 -0500

    added menu_item model functionality

--- app/models/menu_item.py
+++ app/models/menu_item.py
@@ -1,1 +1,13 @@
 
+from .db import db
+
+class MenuItem(db.Model):
+  __tablename__ = 'menu_items'
+
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(50), nullable=False)
+  price = db.Column(db.Integer, nullable=False)
+  image_url = db.Column(db.String(200), nullable=False)
+  business_id = db.Column(db.Integer, db.ForeignKey("businesses.id"),  nullable=False)
+
+  business_menu = db.relationship('Business', back_populates="menu_item")
