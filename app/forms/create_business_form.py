5f7297160cc47b7ec34ef9c7da87417d2e47221e
Tom Bluher <tbluher14@gmail.com>
2022-10-14 16:50:13 -0600

    added create business form

--- app/forms/create_business_form.py
+++ app/forms/create_business_form.py
@@ -1,7 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.validators import DataRequired, Email, ValidationError, Length
 from app.models import User
 
-class CreateBusinessForm():
-  pass
+class CreateBusinessForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=50)])
+    address = StringField('Address', validators=[DataRequired(), Length(min=2, max=50)])
+    city = StringField('City', validators=[DataRequired(), Length(min=2, max=50)])
+    state = StringField('State', validators=[DataRequired(), Length(min=2, max=50)])
+    country = StringField('Country', validators=[DataRequired(), Length(min=2, max=50)])
+    zip_code = StringField('Zip Code', validators=[DataRequired(), Length(5)])
+    website = StringField('Website', validators=[DataRequired(), Length(min=2)])
+    phone = StringField('Phone Number', validators=[DataRequired(), Length(10)])
+    description = StringField('Description', validators=[DataRequired(), Length(min=2)])
+    image = StringField('Image', validators=[DataRequired()])
