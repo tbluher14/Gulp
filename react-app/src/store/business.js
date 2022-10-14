3218ff0ecb20e1c9613295657784ea2677446391
Tom Bluher <tbluher14@gmail.com>
2022-10-14 13:53:40 -0600

    created business reducer

--- react-app/src/store/business.js
+++ react-app/src/store/business.js
@@ -23,4 +23,19 @@ export const getAllSpots = () => async (dispatch) => {
 }
 
 
-//
+// Business Reducer
+const intialState = {};
+const businessReducer = (state = intialState, action) => {
+    let newState;
+    switch (action.type){
+        case GET_ALL_BUSINESSES: {
+            newState = {}
+            action.businesses.forEach(business => newState[business.id] = business)
+            return newState
+        }
+        default:
+            return state
+    }
+}
+
+export default businessReducer
