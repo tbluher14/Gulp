c57c8fd5e3bddacaf72fa8a6ae0e952443991a69
matt-fong <matthewfong12@gmail.com>
2022-10-14 16:05:56 -0400

    added menu items seeders

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,8 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .businesses import seed_businesses, undo_businesses
+from .reviews import seed_reviews, undo_reviews
+from .menu_items import seed_menu_items, undo_menu_items
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -11,6 +14,10 @@ seed_commands = AppGroup('seed')
 def seed():
     seed_users()
     # Add other seed functions here
+    seed_businesses()
+    seed_reviews()
+    seed_menu_items()
+
 
 
 # Creates the `flask seed undo` command
@@ -18,3 +25,6 @@ def seed():
 def undo():
     undo_users()
     # Add other undo functions here
+    undo_businesses()
+    undo_reviews()
+    undo_menu_items()
