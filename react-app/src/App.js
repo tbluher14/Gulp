b1c345f947c61ce5e29f8a414b2fe45509008860
matt-fong <matthewfong12@gmail.com>
2022-10-15 02:04:58 -0400

    Updated Component Names

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,7 +8,7 @@ import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
 import { authenticate } from './store/session';
-import AllBusinesses from './components/AllBusinesses';
+import AllBusinesses from './components/AllBusinesses/AllBusinesses';
 import BusinessCreateForm from './components/NavBar/CreateBusiness';
 
 function App() {
