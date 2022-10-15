df5000fc4dff765fd28e1902f3a9c97b735795c8
matt-fong <matthewfong12@gmail.com>
2022-10-15 16:48:25 -0400

    Added Picture And Link To All Businesses In Home Page CSS

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,10 +1,17 @@
-
+import { NavLink, useHistory } from 'react-router-dom'
+import './HomePage.css'
 
 const HomePage = () => {
+
+  const history = useHistory()
+
   return (
-    <div>
+    <div className='homepage-container'>
       <h1>Home Page</h1>
-      <p>Some content</p>
+      <div className='homepage-picture-container'>
+        <img className='homepage-picture' src='https://www.seriouseats.com/thmb/5avoLwcpCbqBCZX6Gnp24QNIBZ8=/1500x844/smart/filters:no_upscale()/20210607-INNOUTBURGERS-JANJIGIAN-seriouseats-23-b2b8a505ff414272aab71590a8985b85.jpg'></img>
+        <button className="homepage-business-button" onClick={() => history.push('/businesses')}>View All Businesses</button>
+      </div>
     </div>
   );
 }
