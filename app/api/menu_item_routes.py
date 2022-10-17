162cd12c6b791edf71669746314fea3da971fa94
Willard Marett <will.j.marett@gmail.com>
2022-10-17 12:16:45 -0500

    added menu item api route and model dict

--- app/api/menu_item_routes.py
+++ app/api/menu_item_routes.py
@@ -1,4 +1,15 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
 from app.models import User, MenuItem, Business, db
-# from ..forms.menu_item_form import CreateMenuItemForm
+from ..forms.menu_item_form import CreateMenuItemForm
+
+
+menu_item_routes = Blueprint('menu_items', __name__)
+
+#****************************************************************************************************
+# Get Menu Items Route
+@menu_item_routes.route('/', methods=['GET'])
+def get_all_menu_items():
+  menu_items = MenuItem.query.all()
+  print('THIS IS MENU ITEMS', menu_items)
+  return {'menu_items': [menu_item.to_dict() for menu_item in menu_items]}
--- app/models/menu_item.py
+++ app/models/menu_item.py
@@ -11,3 +11,13 @@ class MenuItem(db.Model):
   business_id = db.Column(db.Integer, db.ForeignKey("businesses.id"),  nullable=False)
 
   business_menu = db.relationship('Business', back_populates="menu_item")
+
+
+  def to_dict(self):
+    return {
+      "id" : self.id,
+      "name" : self.name,
+      "price" : self.price,
+      "image_url" : self.image_url,
+      "business_id" : self.business_id,
+    }
