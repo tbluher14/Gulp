88cb617b37998f611d3250e2968de39338ab5fb0
Tom Bluher <tbluher14@gmail.com>
2022-10-18 11:45:39 -0600

    create business is functioning again

--- react-app/src/components/NavBar/CreateBusinessForm.js
+++ react-app/src/components/NavBar/CreateBusinessForm.js
@@ -25,10 +25,13 @@ function BusinessCreateForm() {
   const [errors, setErrors] = useState([]);
   const [isSubmitted, setIsSubmitted] = useState(false);
 
+  // function isImage(image) {
+  //   return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(image)
+  // }
 
-  const handleSubmit = async (e) => {
+
+const handleSubmit = async (e) => {
     e.preventDefault()
-    setErrors([])
 
     const data = {
       owner_id: user.id,
@@ -46,22 +49,94 @@ function BusinessCreateForm() {
       image: image
     };
 
-    
-    return dispatch(createBusinessThunk(data))
-    .then( async (res) => {
-      setIsSubmitted(true)
-      setErrors([])
-    })
-    .catch(async (res) => {
-      console.log("THIS IS RESSSSS", res)
-      const result = res.json()
-      console.log("THIS IS DATA IN CREATE", result)
-      if (result && result.errors){
-        setIsSubmitted(false)
-        setErrors(Object.values(data.errors))
-      }
-    })
-  };
+    let errors = []
+
+    if (!user){
+      errors.push("User must be logged in")
+      setErrors(errors)
+    }
+    else {
+    // if (!isImage(image)) {
+    //   errors.push( "Must be a valid image: jpg, jpeg, png, webp, avif, gif, svg " )
+    // }
+
+    if (name.length < 5 || name.length > 255) {
+      errors.push( "Name must be between 5 to 255 characters." )
+    }
+
+    if (address.length < 5 || address.length > 255) {
+      errors.push( "Address must be between 5 to 255 characters." )
+    }
+
+    if (city.length < 5 || city.length > 255) {
+      errors.push( "City must be between 5 to 255 characters." )
+    }
+
+    if (state.length < 5 || state.length > 255) {
+      errors.push( "State must be between 5 to 255 characters." )
+    }
+
+    if (open < 0 || open > 23){
+      errors.push('Please enter valid opening time')
+    }
+    if (close < 0 || close > 23){
+      errors.push('Please enter valid closing time')
+    }
+
+    if (country.length < 1 || country.length > 255) {
+      errors.push( "Country must be between 1 and 255 characters." )
+    }
+    if (zipCode.length < 5 || zipCode.length > 5) {
+      errors.push( "Country must be 5 characters" )
+    }
+    if (description.length < 5 || description.length > 255) {
+      errors.push( "Description must be between 5 to 255 characters." )
+    }
+    if (phone.length !== 10) {
+      errors.push( "Phone must be 10 characters" )
+    }
+    setErrors(errors)
+  }
+
+  if (user
+  && (name.length > 5 && name.length<255)
+  && (address.length >=5 && address.length <= 255)
+  && (city.length >= 5 && city.length <= 255)
+  && (state.length >= 5 && state.length <= 255)
+  && (open >= 0 && open <= 23)
+  && (close >= 0 && close <= 23)
+  && (country.length >= 5 && country.length <= 255)
+  && (zipCode.length === 5)
+  && (description.length >= 5 && description.length <= 255)
+  && (phone.length === 10)
+  )
+  {
+    return dispatch(createBusinessThunk(data)).then((res) => history.push(`/businesses/${res.id}`)
+    // .catch(
+    //   async (res) => {
+    //     const data = await res.json()
+    //     if (data && data.errors) setErrors(data.errors)
+    //   }
+    // )
+    )
+  }
+}
+
+
+
+  //   return dispatch(createBusinessThunk(data))
+  //   .then( async (res) => {
+  //     setIsSubmitted(true)
+  //     setErrors([])
+  //   })
+  //   .catch(async (res) => {
+  //     const result = res.json()
+  //     if (result && result.errors){
+  //       setIsSubmitted(false)
+  //       setErrors(Object.values(data.errors))
+  //     }
+  //   })
+  // };
 
   return (
     <form onSubmit={handleSubmit}>
@@ -78,7 +153,7 @@ function BusinessCreateForm() {
             <input className="create-business-input"
               type="text"
               value={name}
-              placeholder="name"
+              placeholder="Business Name"
               onChange={(e) => setName(e.target.value)}
               required
             />
@@ -87,7 +162,7 @@ function BusinessCreateForm() {
             <input className="create-business-input"
               type="text"
               value={address}
-              placeholder="address"
+              placeholder="Business Address"
               onChange={(e) => setAddress(e.target.value)}
               required
             />
@@ -96,7 +171,7 @@ function BusinessCreateForm() {
             <input className="create-business-input"
               type="text"
               value={city}
-              placeholder="city"
+              placeholder="City"
               onChange={(e) => setCity(e.target.value)}
               required
             />
@@ -105,7 +180,7 @@ function BusinessCreateForm() {
             <input className="create-business-input"
               type="text"
               value={state}
-              placeholder="state"
+              placeholder="State"
               onChange={(e) => setState(e.target.value)}
               required
             />
@@ -114,7 +189,7 @@ function BusinessCreateForm() {
             <input className="create-business-input"
               type="text"
               value={country}
-              placeholder="country"
+              placeholder="Country"
               onChange={(e) => setCountry(e.target.value)}
               required
             />
@@ -123,7 +198,7 @@ function BusinessCreateForm() {
             <input className="create-business-input"
               type="text"
               value={zipCode}
-              placeholder="zipCode"
+              placeholder="Zip Code"
               onChange={(e) => setZipCode(e.target.value)}
               required
             />
@@ -132,7 +207,7 @@ function BusinessCreateForm() {
             <input className="create-business-input"
               type="text"
               value={website}
-              placeholder="Website"
+              placeholder="Business Website"
               onChange={(e) => setWebsite(e.target.value)}
               required
             />
@@ -141,7 +216,7 @@ function BusinessCreateForm() {
             <input className="create-business-input"
               type="text"
               value={open}
-              placeholder="Open"
+              placeholder="Open Time"
               onChange={(e) => setOpen(e.target.value)}
               required
             />
@@ -150,7 +225,7 @@ function BusinessCreateForm() {
             <input className="create-business-input"
               type="text"
               value={close}
-              placeholder="Close"
+              placeholder="Close Time"
               onChange={(e) => setClose(e.target.value)}
               required
             />
@@ -159,7 +234,7 @@ function BusinessCreateForm() {
             <input className="create-business-input"
               type="text"
               value={phone}
-              placeholder="phone"
+              placeholder="Phone"
               onChange={(e) => setPhone(e.target.value)}
               required
             />
@@ -168,7 +243,7 @@ function BusinessCreateForm() {
             <input className="create-business-input"
               type="text"
               value={description}
-              placeholder="description"
+              placeholder="Business Description"
               onChange={(e) => setDescription(e.target.value)}
               required
             />
@@ -177,7 +252,7 @@ function BusinessCreateForm() {
             <input className="create-business-input"
               type="text"
               value={image}
-              placeholder="image"
+              placeholder="Image"
               onChange={(e) => setImage(e.target.value)}
               required
             />
