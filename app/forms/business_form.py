b189e0567ce737103591d49f5b3fc13489883e17
Tom Bluher <tbluher14@gmail.com>
2022-10-18 08:35:35 -0600

    added business validators and menu item validators

--- app/forms/business_form.py
+++ app/forms/business_form.py
@@ -5,7 +5,7 @@ from app.models import Business
 
 def name_length_check(form, field):
     if len(field.data) < 1 or len(field.data) > 50:
-        raise ValidationError('Name must be between 1 and 50 characters long.')
+        raise ValidationError('Business Name must be between 1 and 50 characters long.')
 
 def address_length_check(form, field):
     if len(field.data) < 1 or len(field.data) > 100:
@@ -24,16 +24,16 @@ def country_length_check(form, field):
         raise ValidationError('Country must be between 1 and 50 characters long.')
 
 def zipCode_length_check(form, field):
-    if len(field.data) < 1 or len(field.data) > 5:
-        raise ValidationError('Zip Code must be between 1 and 5 characters long.')
+    if len(field.data) < 5 or len(field.data) > 5:
+        raise ValidationError('Please enter a valid Zip Code')
 
 def website_length_check(form, field):
     if len(field.data) < 1 or len(field.data) > 500:
         raise ValidationError('Website must be between 1 and 500 characters long.')
 
 def phone_length_check(form, field):
-    if len(field.data) < 1 or len(field.data) > 10:
-        raise ValidationError('Phone must be between 1 and 10 characters long.')
+    if len(field.data) < 10 or len(field.data)>10:
+        raise ValidationError('Phone enter a valid phone number.')
 
 def description_length_check(form, field):
     if len(field.data) < 1 or len(field.data) > 2000:
--- app/forms/menu_item_form.py
+++ app/forms/menu_item_form.py
@@ -11,8 +11,12 @@ def check_image(form, field):
     if not field.data.startswith("https"):
         raise ValidationError("Please enter a valid image url")
 
+def check_name(form, field):
+    if len(form.data)<1 or len(form.data)>255:
+        raise ValidationError('Please enter a valid Menu Item Name between 1 and 255 characters long')
+
 class CreateMenuItemForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(min=1, max=255)])
+    name = StringField('Name', validators=[DataRequired(), Length(min=1, max=255), check_name])
     price = IntegerField('Price', validators=[DataRequired(), check_price])
     image_url = StringField('Image URL', validators=[DataRequired(), Length(min=1), check_image])
     business_id = IntegerField('business_id', validators=[DataRequired()])
