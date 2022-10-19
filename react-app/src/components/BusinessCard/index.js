522734642479e8bb59e29fe7b15c121944a6729a
Tom Bluher <tbluher14@gmail.com>
2022-10-19 17:13:40 -0600

    updates

--- react-app/src/components/BusinessCard/index.js
+++ react-app/src/components/BusinessCard/index.js
@@ -7,6 +7,19 @@ import './BusinessCard.css';
 const BusinessCard = ({ business }) => {
   const history = useHistory()
   const dispatch = useDispatch();
+
+  const reviews = useSelector(state => (state.reviews))
+
+
+  console.log("THIS IS BUSINESS REVIEWS" , reviews)
+
+  // const averageReview = (businessReviewsArray) => {
+  //   let total = 0
+  //   for (let i = 0; i<businessReviewsArray.length; i++){
+  //     total += businessReviewsArray[i].rating
+  //   }
+  //   return (total / businessReviewsArray.length).toFixed(2)
+  // }
  // new
   return (
     <div className='business-card'>
