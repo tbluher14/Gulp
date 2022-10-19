ae89969f55f3291e37d0cb768f52b2583fd5f8d9
Willard Marett <will.j.marett@gmail.com>
2022-10-19 10:42:11 -0500

    initiated css for create business form

--- react-app/src/components/NavBar/CreateBusinessForm.js
+++ react-app/src/components/NavBar/CreateBusinessForm.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { createBusinessThunk } from "../../store/business";
+import "./CreateBusinessForm.css";
 
 function BusinessCreateForm() {
 
