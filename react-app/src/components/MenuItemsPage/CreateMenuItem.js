303c7dba5ea6b8b35719d0677faaf8b66f96eb2e
Tom Bluher <tbluher14@gmail.com>
2022-10-19 10:39:53 -0600

    updates to image logic on menu item

--- react-app/src/components/MenuItemsPage/CreateMenuItem.js
+++ react-app/src/components/MenuItemsPage/CreateMenuItem.js
@@ -14,13 +14,22 @@ const MenuItemCreateForm = () => {
     const [price, setPrice] = useState('')
     const [image_url, setImageUrl] = useState('')
 
+    const imageLogic = (image_url) => {
+        if (image_url){
+            return image_url
+        }
+        else{
+            return "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1398&q=80"
+        }
+    }
+
     const handleSubmit = async (e) => {
         e.preventDefault()
-        console.log("BUSINESS ID", businessId.businessId)
+        
         const data = {
             name: name,
             price: price,
-            image_url: image_url,
+            image_url: imageLogic(image_url),
             business_id: Number(businessId.businessId)
         }
         console.log("THIS IS DATS IN CREATE MENU ITEM", data)
@@ -49,7 +58,7 @@ const MenuItemCreateForm = () => {
                 value={image_url}
                 placeholder={"Item Image URL"}
                 onChange={(e)=> setImageUrl(e.target.value)}
-                required
+
             ></input>
 
             <button name="submit" type="submit">
