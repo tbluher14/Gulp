e653627b68846c1dbafed27eed54fe9580554e54
Willard Marett <will.j.marett@gmail.com>
2022-10-20 18:11:36 -0500

    final updates group 6

--- react-app/src/components/BusinessDetails/EditBusinessForm.js
+++ react-app/src/components/BusinessDetails/EditBusinessForm.js
@@ -16,30 +16,35 @@ function BusinessEditForm() {
   // console.log("this is businessID in react", businessId)
   // console.log("this is business in react", business)
 
-  const [name, setName] = useState(business?.name);
-  const [address, setAddress] = useState(business?.address);
-  const [city, setCity] = useState(business?.city);
-  const [state, setState] = useState(business?.state);
-  const [country, setCountry] = useState(business?.country);
-  const [zipCode, setZipCode] = useState(business?.zipCode);
-  const [website, setWebsite] = useState(business?.website);
-  const [phone, setPhone] = useState(business?.phone);
-  const [description, setDescription] = useState(business?.description);
-  const [open, setOpen] = useState(business?.open)
-  const [close, setClose] = useState(business?.close)
-  const [image, setImage] = useState(business?.image)
-  const [ampmopen, setAmpmopen] = useState(business?.ampmopen)
-  const [ampmclose, setAmpmclose] = useState(business?.ampmclose)
+  const [name, setName] = useState(business?.name || "");
+  const [address, setAddress] = useState(business?.address || "");
+  const [city, setCity] = useState(business?.city || "");
+  const [state, setState] = useState(business?.state || "");
+  const [country, setCountry] = useState(business?.country || "");
+  const [zipCode, setZipCode] = useState(business?.zipCode || "");
+  const [website, setWebsite] = useState(business?.website || "");
+  const [phone, setPhone] = useState(business?.phone || "");
+  const [description, setDescription] = useState(business?.description || "");
+  const [open, setOpen] = useState(business?.open || "")
+  const [close, setClose] = useState(business?.close || "")
+  const [image, setImage] = useState(business?.image || "")
+  const [ampmopen, setAmpmopen] = useState(business?.ampmopen || "")
+  const [ampmclose, setAmpmclose] = useState(business?.ampmclose || "")
 
   const [errors, setErrors] = useState([]);
   const [submitted, setSubmitted] = useState(false);
-  // const [isLoaded, setIsLoaded] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   const imageRegX = /\.(jpeg|jpg|png|svg)$/
   const webRegX = /\.(com|net|org|co|biz|info|gov)$/
   const timeRegX = /^(0?[1-9]|1[0-2]):[0-5][0-9]$/
   const phoneRegX = /^\d{10}$/
   const zipRegX = /^\d{5}$/
+
+  useEffect((e) => {
+    dispatch(getAllBusinessesThunk()).then(() => setIsLoaded(true))
+  }, [])
+
 
   useEffect(() => {
     let errors = []
@@ -52,45 +57,45 @@ function BusinessEditForm() {
       //   errors.push( "Must be a valid image: jpg, jpeg, png, webp, avif, gif, svg " )
       // }
 
+      if (!business) return
 
-
-      if (name.length < 5 || name.length > 255) {
+      if ((name.length < 5 || name.length > 255)) {
         errors.push("Business Name must be between 5 to 255 characters.")
       }
 
-      if (address.length < 5 || address.length > 255) {
+      if ((address.length < 5 || address.length > 255)) {
         errors.push("Business Address must be between 5 to 255 characters.")
       }
 
-      if (city.length < 5 || city.length > 255) {
+      if ((city.length < 5 || city.length > 255)) {
         errors.push("City must be between 5 to 255 characters.")
       }
 
-      if (state.length < 5 || state.length > 255) {
+      if ((state.length < 5 || state.length > 255)) {
         errors.push("State must be between 5 to 255 characters.")
       }
-      if (country.length < 4 || country.length > 255) {
+      if ((country.length < 4 || country.length > 255)) {
         errors.push("Country must be between 4 and 255 characters.")
       }
-      if (website.length < 1 || !website.match(webRegX)) {
+      if ((website.length < 1 || !website.match(webRegX))) {
         errors.push("Business Website must be a valid URL");
       }
-      if (!zipCode.match(zipRegX)) {
+      if ((!zipCode.match(zipRegX))) {
         errors.push("Zipcode must be 5 numbers")
       }
-      if (open < 0 || open > 13 || !open.match(timeRegX)) {
+      if ((open < 0 || open > 13 || !open.match(timeRegX))) {
         errors.push('Please select an opening time')
       }
-      if (close < 0 || close > 13 || !close.match(timeRegX)) {
+      if ((close < 0 || close > 13 || !close.match(timeRegX))) {
         errors.push('Please select a closing time')
       }
-      if (phone.length !== 10 || !phone.match(phoneRegX)) {
+      if ((phone.length !== 10 || !phone.match(phoneRegX))) {
         errors.push("Business Phone must be 10 sequential numbers (ex: 1234567890)")
       }
-      if (description.length < 5 || description.length > 255) {
+      if ((description.length < 5 || description.length > 255)) {
         errors.push("Description must be between 5 to 255 characters.")
       }
-      if (image.length < 1 || !image.split('?')[0].match(imageRegX)) {
+      if ((image.length < 1 || !image.split('?')[0].match(imageRegX))) {
         errors.push("Image must be a valid type: jpg, jpeg, png, or svg");
       }
       setErrors(errors)
@@ -127,7 +132,7 @@ function BusinessEditForm() {
 
   // if (!isLoaded) return null
 
-  return (
+  return isLoaded && (
     <div className="form-outer-container">
       <form onSubmit={handleSubmit}>
         <div className="form-header">Please fill out all of the following fields:</div>
