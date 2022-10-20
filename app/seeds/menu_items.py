a34fec89507d9fef62c0c124447a17f325041f4b
Tom Bluher <tbluher14@gmail.com>
2022-10-20 17:07:17 -0600

    updating seeders

--- app/seeds/menu_items.py
+++ app/seeds/menu_items.py
@@ -6,11 +6,11 @@ x = range(1,32)
 def seed_menu_items():
   for n in x:
     item_one = MenuItem(
-        name='Deluxe Burger', price="10", image_url='https://images.unsplash.com/photo-1632203171982-cc0df6e9ceb4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=528&q=80', business_id=n)
+        name='Deluxe Burger', price="10.00", image_url='https://images.unsplash.com/photo-1632203171982-cc0df6e9ceb4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=528&q=80', business_id=n)
     item_two = MenuItem(
-        name='Bacon Burger', price="15", image_url="https://images.unsplash.com/photo-1572802419224-296b0aeee0d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1115&q=80", business_id=n)
+        name='Bacon Burger', price="15.00", image_url="https://images.unsplash.com/photo-1572802419224-296b0aeee0d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1115&q=80", business_id=n)
     item_three = MenuItem(
-      name='Bacon Burger & Fries', price="20", image_url='https://images.unsplash.com/photo-1561758033-d89a9ad46330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', business_id=n)
+      name='Bacon Burger & Fries', price="20.00", image_url='https://images.unsplash.com/photo-1561758033-d89a9ad46330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', business_id=n)
     db.session.add(item_one)
     db.session.add(item_two)
     db.session.add(item_three)
