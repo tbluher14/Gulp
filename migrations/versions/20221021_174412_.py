76a05fe3dbfbdf859ca7b1bb3dc9ebb77fb9793c
Willard Marett <will.j.marett@gmail.com>
2022-10-21 17:44:56 -0500

    update migrations

--- migrations/versions/20221021_174412_.py
+++ migrations/versions/20221021_174412_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: fd4c5df88ac4
+Revision ID: c62742b8bbd8
 Revises: 
-Create Date: 2022-10-21 17:29:12.517549
+Create Date: 2022-10-21 17:44:12.909667
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'fd4c5df88ac4'
+revision = 'c62742b8bbd8'
 down_revision = None
 branch_labels = None
 depends_on = None
